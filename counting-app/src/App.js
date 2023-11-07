0a12f5dbfb428a583a4e32b91cb6ed7b202fb231
Purvi2401 <purvi2401binani@gmail.com>
2023-11-07 18:39:05 +0530

    authentication and counter

---
+++ counting-app/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import PasswordLoginWithFirebase from "./PasswordLoginWithFirebase/PasswordLoginWithFirebase";
+
+function App() {
+  return (
+    <div>
+      <PasswordLoginWithFirebase />
+    </div>
+  );
+}
+
+export default App;
